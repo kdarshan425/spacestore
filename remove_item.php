2c2201b297ca29f25801111cda291a31f0656e3e
kdarshan425 <kdarshan425@gmail.com>
2021-10-31 15:58:23 +0530

    Update remove_item.php

--- remove_item.php
+++ remove_item.php
@@ -1,5 +1,4 @@
 <?php
-
 require("partials/dbconnect.php");
 session_start();
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
@@ -7,7 +6,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $user_id = $_SESSION['sno'];
     
     // Delete the rows from user_items table where item_id and user_id equal to the item_id and user_id we got from the cart page and session
-    $query = "DELETE FROM `user_items` WHERE `item_id`='$item_id' AND `user_id`='$user_id' ";
+    $query = "DELETE FROM `user_items` WHERE `item_id`='$item_id' AND `user_id`='$user_id' AND `status`='Added to cart' ";
     $res = mysqli_query($conn, $query);
     header("location:cart.php");
 }
